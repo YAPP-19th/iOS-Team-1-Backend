a69b76eefe25a58f041280ade6c1062306a8183e
VIXXPARK <vixx170627@gmail.com>
2021-09-13 20:12:11 +0900

    [YAPPIOS-14] 스웨거 사용 시 헤더 넣는 부분 없어서 추가

--- project/src/main/java/com/yapp/project/config/SwaggerConfig.java
+++ project/src/main/java/com/yapp/project/config/SwaggerConfig.java
@@ -3,14 +3,19 @@ package com.yapp.project.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Configuration
@@ -37,7 +42,19 @@ public class SwaggerConfig {
     }
     @Bean
     public Docket commonApi() {
+        ParameterBuilder parameterBuilder = new ParameterBuilder();
+
+        parameterBuilder.name("Authorization")
+                .description("Access Token")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
+
+        List<Parameter> parameters = new ArrayList<>();
+        parameters.add(parameterBuilder.build());
         return new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(parameters)
                 .consumes(getConsumeContentTypes())
                 .produces(getProduceContentTypes())
                 .apiInfo(apiInfo())
