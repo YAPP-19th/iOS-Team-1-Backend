98ba2c500b8a47095d7a078178ebe20c2f482f37
VIXXPARK <vixx170627@gmail.com>
2021-09-13 20:12:30 +0900

    [YAPPIOS-14] 레디스로 변환하면서 jpa에 필요한 메소드 삭제 .

--- project/src/main/java/com/yapp/project/account/domain/RefreshToken.java
+++ project/src/main/java/com/yapp/project/account/domain/RefreshToken.java
@@ -1,15 +1,14 @@
 package com.yapp.project.account.domain;
 
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
 
 
 @Getter
-@NoArgsConstructor
+@RequiredArgsConstructor
 @RedisHash("refreshToken")
 public class RefreshToken {
 
@@ -17,15 +16,4 @@ public class RefreshToken {
     private String key;
     @Indexed
     private String value;
-
-    public RefreshToken updateValue(String value){
-        this.value = value;
-        return this;
-    }
-
-    @Builder
-    public RefreshToken(String key, String value){
-        this.key = key;
-        this.value = value;
-    }
 }
