758b6acef5ffdecd0538eae4c3d4d0cd4c7df308
VIXXPARK <vixx170627@gmail.com>
2021-09-13 15:14:29 +0900

    [YAPPIOS-14] Swagger 적용

--- project/src/main/java/com/yapp/project/account/controller/AccountController.java
+++ project/src/main/java/com/yapp/project/account/controller/AccountController.java
@@ -2,6 +2,7 @@ package com.yapp.project.account.controller;
 
 import com.yapp.project.account.domain.dto.AccountResponseDto;
 import com.yapp.project.account.service.AccountService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
     private final AccountService accountService;
 
+    @ApiOperation(value = "회원정보", tags = "account-controller")
     @GetMapping("/me")
     public ResponseEntity<AccountResponseDto> getMyAccountInfo() {
         return ResponseEntity.ok(accountService.getUserInfo());
     }
 
+    @ApiOperation(value = "이메일로 회원정보 보기", tags = "account-controller")
     @GetMapping("/{email}")
     public ResponseEntity<AccountResponseDto> getAccountInfo(@PathVariable String email){
         return ResponseEntity.ok(accountService.getAccountInfo(email));
--- project/src/main/java/com/yapp/project/account/controller/AuthController.java
+++ project/src/main/java/com/yapp/project/account/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.yapp.project.account.domain.dto.AccountResponseDto;
 import com.yapp.project.account.domain.dto.TokenDto;
 import com.yapp.project.account.domain.dto.TokenRequestDto;
 import com.yapp.project.account.service.AuthService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,16 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
 
+    @ApiOperation(value = "회원가입", tags = "auth-controller")
     @PostMapping("/signup")
     public ResponseEntity<AccountResponseDto> signup(@RequestBody AccountRequestDto accountRequestDto){
         return ResponseEntity.ok(authService.signup(accountRequestDto));
     }
 
+    @ApiOperation(value = "로그인", tags = "auth-controller")
     @PostMapping("/login")
     public ResponseEntity<TokenDto> login(@RequestBody AccountRequestDto accountRequestDto){
         return ResponseEntity.ok(authService.login(accountRequestDto));
     }
 
+    @ApiOperation(value = "토큰 재발행", tags = "auth-controller")
     @PostMapping("/reissue")
     public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto){
         return ResponseEntity.ok(authService.reissue(tokenRequestDto));
