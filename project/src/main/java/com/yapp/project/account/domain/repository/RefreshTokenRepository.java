2d064eab40540c483815f652305b348fa577e4c4
VIXXPARK <vixx170627@gmail.com>
2021-09-13 09:24:18 +0900

    [YAPPIOS-14] 현재 리플래시 토큰은 jpa를 통해 구현 중이지만 redis로 변경할 예정

---
+++ project/src/main/java/com/yapp/project/account/domain/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.yapp.project.account.domain.repository;
+
+import com.yapp.project.account.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByKey(String key);
+
+}
