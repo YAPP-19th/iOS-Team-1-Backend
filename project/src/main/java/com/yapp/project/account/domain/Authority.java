36be78b3fa5dc68c645f40b4165a119a03b49559
VIXXPARK <vixx170627@gmail.com>
2021-09-13 15:30:49 +0900

    [YAPPIOS-14] gitignore 실수로 인한 롤백.

--- project/src/main/java/com/yapp/project/account/domain/Authority.java
+++
@@ -1,5 +0,0 @@
-package com.yapp.project.account.domain;
-
-public enum Authority {
-    ROLE_USER,ROLE_STAFF,ROLE_ADMIN
-}
--- project/src/main/java/com/yapp/project/account/domain/repository/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yapp.project.account.domain.repository;
-
-import com.yapp.project.account.domain.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
-}
