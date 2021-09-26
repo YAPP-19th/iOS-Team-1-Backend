c2edb8b83d3cb4532edae630da345cee95a1aec8
VIXXPARK <vixx170627@gmail.com>
2021-09-26 14:55:17 +0900

    [YAPPIOS-2O] 생성자 추가

--- project/src/main/java/com/yapp/project/account/domain/dto/TokenRequestDto.java
+++ project/src/main/java/com/yapp/project/account/domain/dto/TokenRequestDto.java
@@ -1,10 +1,12 @@
 package com.yapp.project.account.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class TokenRequestDto {
     private String refreshToken;
 }
