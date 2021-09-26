0976e10e2ae7d5d123e5b4ea8d2303c51f64ed16
VIXXPARK <vixx170627@gmail.com>
2021-09-26 14:56:40 +0900

    [YAPPIOS-2O] 추가로 테스트 템플릿 확장에 따른 분리 작업

--- project/src/main/java/com/yapp/project/aux/test/account/AccountTemplate.java
+++ project/src/main/java/com/yapp/project/aux/test/account/AccountTemplate.java
@@ -1,30 +1,40 @@
-package com.yapp.project.aux;
+package com.yapp.project.aux.test.account;
 
 import com.yapp.project.account.domain.Account;
 import com.yapp.project.account.domain.Authority;
+import com.yapp.project.account.domain.dto.AccountRequestDto;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.time.LocalDateTime;
 
-public class Common {
+public class AccountTemplate {
 
-    private Common() {
+    private AccountTemplate() {
         throw new IllegalArgumentException();
     }
 
     private static final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
     private static Long id = 1L;
-    private static final String PASSWORD = "test1234";
+    public static final String PASSWORD = "test1234";
+    public static final String USERNAME = "스프링";
+    public static final String EMAIL = "springboot@example.com";
+    public static final String FIRST_USERNAME = "first";
+    public static final String FIRST_EMAIL = "first@example.com";
+
+
+    public static AccountRequestDto makeFirstUserRequestDto(){
+        return new AccountRequestDto(FIRST_EMAIL,FIRST_USERNAME,PASSWORD);
+    }
 
     public static Account makeTestAccount(){
-        return Account.builder().id(id++).username("스프링").password(bCryptPasswordEncoder.encode(PASSWORD))
-                .email("springboot@example.com").createdAt(LocalDateTime.now()).lastLogin(LocalDateTime.now())
+        return Account.builder().id(id++).username(USERNAME).password(bCryptPasswordEncoder.encode(PASSWORD))
+                .email(EMAIL).createdAt(LocalDateTime.now()).lastLogin(LocalDateTime.now())
                 .authority(Authority.ROLE_USER).build();
     }
 
     public static Account makeTestAccount(String username){
         return Account.builder().id(id++).username(username).password(bCryptPasswordEncoder.encode(PASSWORD))
-                .email("springboot@example.com").createdAt(LocalDateTime.now()).lastLogin(LocalDateTime.now())
+                .email(EMAIL).createdAt(LocalDateTime.now()).lastLogin(LocalDateTime.now())
                 .authority(Authority.ROLE_USER).build();
     }
 
@@ -38,6 +48,18 @@ public class Common {
         return Account.builder().id(id++).username(username).password(bCryptPasswordEncoder.encode(PASSWORD))
                 .email(email).createdAt(LocalDateTime.now()).lastLogin(LocalDateTime.now())
                 .authority(authority).build();
+    }
+
+    public static AccountRequestDto makeAccountRequestDto(){
+        return new AccountRequestDto(EMAIL,USERNAME,PASSWORD);
+    }
+
+    public static AccountRequestDto makeAccountRequestDto(String email){
+        return new AccountRequestDto(email,USERNAME,PASSWORD);
+    }
+
+    public static AccountRequestDto makeAccountRequestDto(String email,String username){
+        return new AccountRequestDto(email,username,PASSWORD);
     }
 
 }
