be58c67c5d261523b0518bfc041c53d47d0c9cc5
solchan98 <dev.pinetree@gmail.com>
2021-10-10 22:39:08 +0900

    [YAPPIOS-45] Routine 생성 요청과 응답 DTO 및 Repository 작성

--- src/main/java/com/yapp/project/routine/domain/RoutineRepository.java
+++ src/main/java/com/yapp/project/routine/domain/RoutineRepository.java
@@ -1,6 +1,5 @@
-package com.yapp.project.routine.service;
+package com.yapp.project.routine.domain;
 
-import com.yapp.project.routine.domain.Routine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
