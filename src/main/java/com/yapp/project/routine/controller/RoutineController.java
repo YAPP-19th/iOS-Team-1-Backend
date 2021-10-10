a02e2660f87bd73771115afead3a38329772a128
solchan98 <dev.pinetree@gmail.com>
2021-10-10 22:40:44 +0900

    [YAPPIOS-45] Routine 생성 Controller 매핑 메서드 작성 및 Service 비즈니스 로직 작성(유저 정보 얻기 수정 예정)

--- src/main/java/com/yapp/project/routine/controller/RoutineController.java
+++ src/main/java/com/yapp/project/routine/controller/RoutineController.java
@@ -1,2 +1,20 @@
-package com.yapp.project.routine.controller;public class RoutineController {
+package com.yapp.project.routine.controller;
+
+import com.yapp.project.routine.domain.RoutineDTO;
+import com.yapp.project.routine.service.RoutineService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/routine")
+public class RoutineController {
+
+    private final RoutineService routineService;
+
+    @PostMapping("/")
+    public RoutineDTO.Response createRoutine(@RequestBody RoutineDTO.Request newRoutine) throws BindException {
+        return routineService.createRoutine(newRoutine);
+    }
 }
--- src/main/java/com/yapp/project/routine/service/RoutineService.java
+++ src/main/java/com/yapp/project/routine/service/RoutineService.java
@@ -1,2 +1,56 @@
-package com.yapp.project.routine.service;public class RoutineService {
+package com.yapp.project.routine.service;
+
+import com.yapp.project.account.domain.Account;
+import com.yapp.project.account.domain.repository.AccountRepository;
+import com.yapp.project.base.Cron;
+import com.yapp.project.routine.domain.Routine;
+import com.yapp.project.routine.domain.RoutineDTO;
+import com.yapp.project.routine.domain.RoutineRepository;
+import com.yapp.project.routine.domain.Week;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class RoutineService {
+
+    private final RoutineRepository routineRepository;
+    private final AccountRepository accountRepository; // will delete
+
+    public RoutineDTO.Response createRoutine(RoutineDTO.Request newRoutine) throws BindException {
+        checkDataIsNull(newRoutine);
+        Routine routine = Routine.builder()
+                .account(getAccount())
+                .newRoutine(newRoutine).build();
+        setCron(newRoutine.getDays(), routine);
+
+        return RoutineDTO.Response.builder()
+                .routine(routineRepository.save(routine)).build();
+    }
+
+    private Boolean checkDataIsNull(RoutineDTO.Request newRoutine) throws BindException {
+        if( newRoutine.getTitle().isBlank() ||
+            newRoutine.getGoal().isBlank() ||
+            newRoutine.getDays().isEmpty() ||
+            newRoutine.getStartTime().isBlank()) throw new BindException(" ","Routine");
+        return true;
+    }
+
+    private void setCron(List<Week> days, Routine routine) {
+        List<Cron> crons = days.stream().map(cron -> Cron.builder().week(cron).build()).collect(Collectors.toList());
+        crons.stream().forEach(cron -> routine.addCrons(cron));
+    }
+
+    // Todo - id Security Context에서 얻는 작업 마무리 되면 수정 예정
+    private Account getAccount() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        Account account =  accountRepository.findByEmail(email).get();
+        return account;
+    }
 }
