a420e4e80ce0f5fd3186c319a89960adc1ca6df6
VIXXPARK <vixx170627@gmail.com>
2021-10-10 12:16:30 +0900

    [YAPPIOS-41] Orgainzation Domain - 기존의 기획의 변화에 따라 칼럼 변경 - 수행 인원 체크하는 칼럼 추가 - capture 에서 shoot으로 변경( capture라는 도메인이 있어서)

--- src/main/java/com/yapp/project/organization/domain/Organization.java
+++ src/main/java/com/yapp/project/organization/domain/Organization.java
@@ -1,50 +1,37 @@
 package com.yapp.project.organization.domain;
 
 import lombok.*;
-import org.springframework.util.Assert;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
 @ToString
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Organization {
-
-    @Builder
-    public Organization(String title, Theme theme, String image, String recommend, String capture, String promise, Double rate){
-        Assert.hasText(title,"제목은 반드시 있어야 합니다.");
-        this.title = title;
-        this.theme = theme;
-        this.image = image;
-        this.recommend = recommend;
-        this.capture = capture;
-        this.promise = promise;
-        this.rate = rate;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany(mappedBy = "organization",cascade = CascadeType.ALL)
-    private final List<Participant> participants = new ArrayList<>();
-
     private String title;
 
-    @Enumerated(EnumType.STRING)
-    private Theme theme;
+    private Double rate;
 
     private String image;
 
     private String recommend;
 
-    private String capture;
+    private String shoot;
 
     private String promise;
 
-    private Double rate;
+    private String category;
+
+    private Long count;
+
+
 
 }
--- src/main/java/com/yapp/project/organization/domain/dto/OrganizationDto.java
+++ src/main/java/com/yapp/project/organization/domain/dto/OrganizationDto.java
@@ -1,11 +1,8 @@
 package com.yapp.project.organization.domain.dto;
 
-import com.yapp.project.routine.domain.Week;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.util.List;
 
 public class OrganizationDto {
     private OrganizationDto(){
@@ -14,27 +11,8 @@ public class OrganizationDto {
     @Getter
     @AllArgsConstructor
     @Builder
-    public static class FindThemeRequest {
-        private String theme;
-    }
-
-    @Getter
-    @AllArgsConstructor
-    @Builder
     public static class DetailsRequest{
         private Long id;
     }
-
-    @Getter
-    @AllArgsConstructor
-    @Builder
-    public static class JoinRequest{
-        private Integer term;
-        private String theme;
-        private Boolean notification;
-        private List<Week> days;
-        // 미정
-    }
-
 
 }
