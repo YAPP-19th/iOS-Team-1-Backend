7d6c02a2da73240023c80b3c0c2074cfb5d6d6a7
solchan98 <dev.pinetree@gmail.com>
2021-10-11 23:54:27 +0900

    [YAPPIOS-46] 테스트 케이스 @SpringBootTest -> Mockito로 변경

--- src/test/java/com/yapp/project/routine/RoutineServiceTest.java
+++ src/test/java/com/yapp/project/routine/RoutineServiceTest.java
@@ -1,55 +1,48 @@
 package com.yapp.project.routine;
 
-import com.yapp.project.account.domain.dto.TokenDto;
-import com.yapp.project.account.service.AuthService;
+import com.yapp.project.account.domain.Account;
 import com.yapp.project.aux.test.account.AccountTemplate;
-import com.yapp.project.config.jwt.TokenProvider;
+import com.yapp.project.routine.domain.Routine;
 import com.yapp.project.routine.domain.RoutineDTO;
+import com.yapp.project.routine.domain.RoutineRepository;
 import com.yapp.project.routine.domain.Week;
 import com.yapp.project.routine.service.RoutineService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.validation.BindException;
-
-import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class RoutineServiceTest {
 
-    @Autowired
-    private AuthService authService;
-    @Autowired
+    @InjectMocks
     private RoutineService routineService;
-    @Autowired
-    private TokenProvider tokenProvider;
 
-    @BeforeEach
-    public void setup() {
-        authService.signup(AccountTemplate.makeAccountRequestDto());
-        TokenDto tokenDto = authService.login(AccountTemplate.makeAccountRequestDto());
-        Authentication authentication = tokenProvider.getAuthentication(tokenDto.getAccessToken());
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-    }
+    @Mock
+    private RoutineRepository routineRepository;
 
     @Test
-    @Transactional
     void Test_Create_Routine_Success() {
         // given
+        Account account = AccountTemplate.makeTestAccount();
         List<Week> days = new ArrayList<>();
         days.add(Week.MON);
         days.add(Week.TUE);
         RoutineDTO.RequestRoutineDto newRoutine = new RoutineDTO.RequestRoutineDto("타이틀", "목포", days, "07:35", "생활");
+        Routine fakeRoutine = Routine.builder().account(account).newRoutine(newRoutine).build();
+
+        // mocking
+        given(routineRepository.save(any())).willReturn(fakeRoutine);
         // when
         try {
-            RoutineDTO.ResponseRoutineDto routine = routineService.createRoutine(newRoutine);
+            RoutineDTO.ResponseRoutineDto routine = routineService.createRoutine(newRoutine, account);
             // then
             assertThat(routine.getTitle()).isEqualTo(newRoutine.getTitle());
             assertThat(routine.getGoal()).isEqualTo(newRoutine.getGoal());
@@ -61,16 +54,16 @@ public class RoutineServiceTest {
     }
 
     @Test
-    @Transactional
     void Test_Create_Routine_Failure() {
         // given
+        Account account = AccountTemplate.makeTestAccount();
         List<Week> days = new ArrayList<>();
         days.add(Week.MON);
         days.add(Week.TUE);
-        RoutineDTO.RequestRoutineDto newRoutine = new RoutineDTO.RequestRoutineDto("", "목포", days, "07:35", "생활");
+        RoutineDTO.RequestRoutineDto newRoutine = new RoutineDTO.RequestRoutineDto("", "", days, "07:35", "생활");
         // when
         try {
-            routineService.createRoutine(newRoutine);
+            routineService.createRoutine(newRoutine, account);
         } catch (BindException e) {
             // then
             assertThat(e).isNotNull();
