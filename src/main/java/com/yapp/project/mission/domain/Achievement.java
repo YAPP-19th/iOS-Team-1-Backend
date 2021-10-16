db07085e089cf6e8c7f9ed26c1726cb752831c6a
VIXXPARK <vixx170627@gmail.com>
2021-10-16 20:54:06 +0900

    [YAPPIOS-75] 모델링 재작업

---
+++ src/main/java/com/yapp/project/mission/domain/Achievement.java
@@ -0,0 +1,12 @@
+package com.yapp.project.mission.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Achievement {
+    private Integer myAchievementRate;
+
+    private Integer groupAchievementRate;
+}
