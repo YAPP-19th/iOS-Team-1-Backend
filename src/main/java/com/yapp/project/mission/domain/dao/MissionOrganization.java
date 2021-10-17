f0f53b971c1657a471f5cab8bf4fc79cee9441cd
VIXXPARK <vixx170627@gmail.com>
2021-10-17 20:37:42 +0900

    [YAPPIOS-66] 미션 관련 작업 - 특정 필드 가져오기 위해 인터페이스 생성 - 계정 정보로 현재 남아 있는 유저 미션 조회 - 미션 내용 가져오기 위해 계정 정보와 미션 아이디로 조회 - 미션을 생성할 때 이미 존재하는 확인하기 위해 조회

---
+++ src/main/java/com/yapp/project/mission/domain/dao/MissionOrganization.java
@@ -0,0 +1,7 @@
+package com.yapp.project.mission.domain.dao;
+
+import com.yapp.project.organization.domain.Organization;
+
+public interface MissionOrganization {
+    Organization getOrganization();
+}
