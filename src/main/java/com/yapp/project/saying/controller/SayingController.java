3e101c3d2680500094c0f1e10d96378db7dab20c
VIXXPARK <vixx170627@gmail.com>
2021-10-20 21:36:05 +0900

    [YAPPIOS-80] 명언 오늘 했는 지 확인하는 기능 추가 - 메세지로 변환하는 작업 dto에 메소드로 변환 - 테스트케이스 추가

--- src/main/java/com/yapp/project/saying/controller/SayingController.java
+++ src/main/java/com/yapp/project/saying/controller/SayingController.java
@@ -1,9 +1,9 @@
 package com.yapp.project.saying.controller;
 
-import com.yapp.project.aux.Message;
-import com.yapp.project.aux.StatusEnum;
+import com.yapp.project.account.domain.Account;
 import com.yapp.project.aux.common.AccountUtil;
-import com.yapp.project.saying.domain.dto.SayingDto;
+import static com.yapp.project.saying.domain.dto.SayingDto.*;
+
 import com.yapp.project.saying.service.SayingService;
 import com.yapp.project.saying.utils.SayingUtils;
 import io.swagger.annotations.ApiOperation;
@@ -21,17 +21,23 @@ public class SayingController {
 
     @ApiOperation(value = "오늘의 사용자 명언", tags = "saying-controller")
     @GetMapping("/today")
-    public SayingDto.SayingAccessMessage randomSaying(){
-        return SayingDto.SayingAccessMessage.builder()
-                .data(sayingService.randomSaying(AccountUtil.getAccount(), SayingUtils.randomSayingId()))
-                .message(Message.builder().status(StatusEnum.SAYING_OK).msg("명언 가져오기 성공").build()).build() ;
+    public SayingAccessMessage randomSaying(){
+        int randomSayingId = SayingUtils.randomSayingId();
+        Account account = AccountUtil.getAccount();
+        return sayingService.randomSaying(account, randomSayingId);
     }
 
-    @ApiOperation(value = "오늘의 명언 제대로 썼는지 확인", tags = "saying-controller")
+    @ApiOperation(value = "클라이언트에서 보내 준 오늘의 명언 제대로 썼는지 확인", tags = "saying-controller")
     @PostMapping("/check")
-    public SayingDto.SayingResponseMessage checkResult(SayingDto.SayingAccess request){
-        return SayingDto.SayingResponseMessage.builder()
-                .data(sayingService.checkResult(request, AccountUtil.getAccount()))
-                .message(Message.builder().status(StatusEnum.SAYING_OK).msg("명언 확인 성공").build()).build();
+    public SayingResponseMessage checkResult(SayingAccess request){
+        Account account = AccountUtil.getAccount();
+        return sayingService.checkResult(request, account);
+    }
+
+    @ApiOperation(value = "명언 기록을 읽어서 오늘 썼는 지 확인", tags = "saying-controller")
+    @GetMapping("/check")
+    public SayingRecordResponseMessage isTodaySayingRecord(){
+        Account account = AccountUtil.getAccount();
+        return sayingService.isTodayRecording(account);
     }
 }
