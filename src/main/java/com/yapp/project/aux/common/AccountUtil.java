fd63fd12d84970dbfdbf06dad923766f5caea907
VIXXPARK <vixx170627@gmail.com>
2021-10-29 21:38:41 +0900

    [YAPPIOS-102] 유저 삭제, 프로필 내에서 비밀번호 변경 - 유저 삭제를 md5로 암호화하여 삭제하는 것으로 진행 - 미션, 캡처 관련 삭제는 isDelete로 처리하여 실시하였고, CaptureImage는 완전 삭제 진행 - 유저 삭제하면서 리포트, 루틴 관련 기능 삭제 처리는 처리해야함 - 테스트케이스 추가

--- src/main/java/com/yapp/project/aux/common/AccountUtil.java
+++ src/main/java/com/yapp/project/aux/common/AccountUtil.java
@@ -4,9 +4,29 @@ import com.yapp.project.account.domain.Account;
 import com.yapp.project.config.security.PrincipalDetails;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class AccountUtil {
+    private AccountUtil(){
+    }
     public static Account getAccount() {
         PrincipalDetails principal = (PrincipalDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return principal.getAccount();
+    }
+
+    public static String generateMD5(String email) throws NoSuchAlgorithmException {
+        StringBuilder stringBuffer = new StringBuilder();
+
+        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+        messageDigest.update(email.getBytes());
+
+        byte[] msgStr = messageDigest.digest();
+
+        for (byte b : msgStr) {
+            String tmpEncTxt = Integer.toHexString(b & 0x00FF);
+            stringBuffer.append(tmpEncTxt);
+        }
+        return stringBuffer.toString();
     }
 }
