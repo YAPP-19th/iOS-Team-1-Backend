9a43eff8fa7d5d267e9f662ed78775670e7bba2d
solchan98 <dev.sol0427@gmail.com>
2021-11-02 03:38:43 +0900

    [YAPPIOS-101] 주리포트 디자인 변경으로 인한 로직 수정

--- src/main/java/com/yapp/project/report/domain/RoutineResult.java
+++ src/main/java/com/yapp/project/report/domain/RoutineResult.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,9 +19,6 @@ public class RoutineResult{
 
     @ManyToOne
     private WeekReport weekReport;
-
-    @OneToMany(mappedBy = "routineResult", cascade = CascadeType.ALL)
-    private List<RoutineReportDay> routineReportDayList = new ArrayList<>();
 
     @OneToMany(mappedBy = "routineResult", cascade = CascadeType.ALL)
     private List<RetrospectReportDay> retrospectReportDays = new ArrayList<>();
@@ -41,21 +39,20 @@ public class RoutineResult{
 
     private Long passDaysCount;
 
+    private LocalDateTime routineCreateAt;
+
     @Builder
-    public RoutineResult(String title, String category, Long routineId, int passDaysCount, int allCount) {
+    public RoutineResult(String title, String category, Long routineId, int passDaysCount, int allCount, LocalDateTime routineCreateAt) {
         this.title = title;
         this.category = category;
         this.routineId = routineId;
         this.passDaysCount = Long.valueOf(passDaysCount);
         this.allCount = Long.valueOf(allCount);
+        this.routineCreateAt = routineCreateAt;
     }
 
     public void addRetrospectDay(RetrospectReportDay retrospectReportDay) {
         this.retrospectReportDays.add(retrospectReportDay);
-    }
-
-    public void addRoutineReportDay(RoutineReportDay routineReportDay) {
-        this.routineReportDayList.add(routineReportDay);
     }
 
     public void addWeekReport(WeekReport weekReport) {
