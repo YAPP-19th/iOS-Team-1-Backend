ba7e50ab891d556fbe911b7ece2eca75edb29560
solchan98 <dev.sol0427@gmail.com>
2021-11-11 14:25:07 +0900

    [YAPPIOS-122] 월 리포트 발급 배치 구현

--- src/main/java/com/yapp/project/batch/job/ReportConfig.java
+++ src/main/java/com/yapp/project/batch/job/ReportConfig.java
@@ -30,7 +30,6 @@ public class ReportConfig {
     private final StepBuilderFactory stepBuilderFactory;
     private final JobBuilderFactory jobBuilderFactory;
     private final AlertService alertService;
-
     private final AccountRepository accountRepository;
     private final ReportService reportService;
 
@@ -43,21 +42,40 @@ public class ReportConfig {
 
     }
 
+    @Bean(name = "makeMonthReportJob")
+    public Job makeMonthReportJob() {
+        return jobBuilderFactory.get("makeMonthReportJob")
+                .start(makeWeekReportStep())
+                .incrementer(new RunIdIncrementer())
+                .build();
+
+    }
+
     @Bean
     public Step makeWeekReportStep() {
         return stepBuilderFactory.get("makeWeekReportStep")
                 .<Account, Account> chunk(10)
-                .reader(makeWeekReportReader())
+                .reader(makeReportReader())
                 .processor(makeWeekReportProcessor())
-                .writer(makeWeekReportWriter())
+                .writer(makeReportWriter())
+                .build();
+    }
+
+    @Bean
+    public Step makeMonthReportStep() {
+        return stepBuilderFactory.get("makeMonthReportStep")
+                .<Account, Account> chunk(10)
+                .reader(makeReportReader())
+                .processor(makeMonthReportProcessor())
+                .writer(makeReportWriter())
                 .build();
     }
 
     @Bean
     @StepScope
-    public ListItemReader<Account> makeWeekReportReader() {
+    public ListItemReader<Account> makeReportReader() {
         List<Account> accountList = accountRepository.findAll();
-        alertService.slackSendMessage(SlackChannel.BATCH, "총 " + accountList.size() + "명의 주 리포트를 발급합니다.");
+        alertService.slackSendMessage(SlackChannel.BATCH, "총 " + accountList.size() + "명의 리포트를 발급합니다.");
         return new ListItemReader<>(accountList);
     }
 
@@ -73,7 +91,20 @@ public class ReportConfig {
         return itemProcessor;
     }
 
-    public ItemWriter<Account> makeWeekReportWriter() {
+    public ItemProcessor<Account, Account> makeMonthReportProcessor() {
+        ItemProcessor<Account, Account> itemProcessor = new ItemProcessor<>() {
+            @Override
+            public Account process(Account account) throws Exception {
+                reportService.makeMonthReport(account);
+                log.info("이메일: " + account.getEmail() + " 발급 완료");
+                return null;
+            }
+        };
+        return itemProcessor;
+    }
+
+
+    public ItemWriter<Account> makeReportWriter() {
         ItemWriter<Account> itemWriter = new ItemWriter<>() {
             @Override
             public void write(List<? extends Account> items) throws Exception {
--- src/main/java/com/yapp/project/batch/scheduler/ReportScheduler.java
+++ src/main/java/com/yapp/project/batch/scheduler/ReportScheduler.java
@@ -13,24 +13,38 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ReportScheduler {
-    private final Job missionJob;
+    private final Job weekReportJob;
+    private final Job monthReportJob;
     private final JobLauncher jobLauncher;
     private final AlertService alertService;
 
-    public ReportScheduler(JobLauncher jobLauncher, @Qualifier("makeWeekReportJob") Job weekReportJob, AlertService alertService){
-        this.missionJob = weekReportJob;
+    public ReportScheduler(JobLauncher jobLauncher, @Qualifier("makeWeekReportJob") Job weekReportJob,
+                           @Qualifier("makeMonthReportJob") Job monthReportJob, AlertService alertService){
+        this.weekReportJob = weekReportJob;
+        this.monthReportJob = monthReportJob;
         this.jobLauncher = jobLauncher;
         this.alertService = alertService;
     }
 
     @Scheduled(cron = "0 0 17 * * 3")
-    public void missionBatchExecuteJob() throws JobExecutionException{
+    public void makeWeekReportJob() throws JobExecutionException{
         alertService.slackSendMessage(SlackChannel.BATCH,
                 ":arrow_forward:주 리포트 발급 배치작업 시작합니다.");
-        jobLauncher.run(missionJob, new JobParametersBuilder()
+        jobLauncher.run(weekReportJob, new JobParametersBuilder()
                 .addString("makeWeekReportDate", DateUtil.KST_LOCAL_DATETIME_NOW().toString())
                 .toJobParameters());
         alertService.slackSendMessage(SlackChannel.BATCH,
                 ":ballot_box_with_check:주 리포트 발급 배치작업 마쳤습니다.");
+    }
+
+    @Scheduled(cron = "0 0 18 1 * ?")
+    public void makeMonthReportJob() throws JobExecutionException{
+        alertService.slackSendMessage(SlackChannel.BATCH,
+                ":arrow_forward:월 리포트 발급 배치작업 시작합니다.");
+        jobLauncher.run(monthReportJob, new JobParametersBuilder()
+                .addString("makeWeekReportDate", DateUtil.KST_LOCAL_DATETIME_NOW().toString())
+                .toJobParameters());
+        alertService.slackSendMessage(SlackChannel.BATCH,
+                ":ballot_box_with_check:월 리포트 발급 배치작업 마쳤습니다.");
     }
 }
