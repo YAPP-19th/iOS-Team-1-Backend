4682e9f5f424b82d84e7a72916edf22e7f739a96
solchan98 <dev.sol0427@gmail.com>
2021-11-11 14:12:31 +0900

    [YAPPIOS-122] 주 리포트 발급 배치 구현

--- src/main/java/com/yapp/project/batch/job/ReportConfig.java
+++ src/main/java/com/yapp/project/batch/job/ReportConfig.java
@@ -1,2 +1,85 @@
-package com.yapp.project.batch.job;public class ReportConfig {
+package com.yapp.project.batch.job;
+
+import com.yapp.project.account.domain.Account;
+import com.yapp.project.account.domain.repository.AccountRepository;
+import com.yapp.project.aux.alert.AlertService;
+import com.yapp.project.aux.alert.SlackChannel;
+import com.yapp.project.report.service.ReportService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.List;
+
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+@EnableBatchProcessing
+public class ReportConfig {
+    private final StepBuilderFactory stepBuilderFactory;
+    private final JobBuilderFactory jobBuilderFactory;
+    private final AlertService alertService;
+
+    private final AccountRepository accountRepository;
+    private final ReportService reportService;
+
+    @Bean(name = "makeWeekReportJob")
+    public Job makeWeekReportJob() {
+        return jobBuilderFactory.get("makeWeekReportJob")
+                .start(makeWeekReportStep())
+                .incrementer(new RunIdIncrementer())
+                .build();
+
+    }
+
+    @Bean
+    public Step makeWeekReportStep() {
+        return stepBuilderFactory.get("makeWeekReportStep")
+                .<Account, Account> chunk(10)
+                .reader(makeWeekReportReader())
+                .processor(makeWeekReportProcessor())
+                .writer(makeWeekReportWriter())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public ListItemReader<Account> makeWeekReportReader() {
+        List<Account> accountList = accountRepository.findAll();
+        alertService.slackSendMessage(SlackChannel.BATCH, "총 " + accountList.size() + "명의 주 리포트를 발급합니다.");
+        return new ListItemReader<>(accountList);
+    }
+
+    public ItemProcessor<Account, Account> makeWeekReportProcessor() {
+        ItemProcessor<Account, Account> itemProcessor = new ItemProcessor<>() {
+            @Override
+            public Account process(Account account) throws Exception {
+                reportService.makeWeekReport(account);
+                log.info("이메일: " + account.getEmail() + " 발급 완료");
+                return null;
+            }
+        };
+        return itemProcessor;
+    }
+
+    public ItemWriter<Account> makeWeekReportWriter() {
+        ItemWriter<Account> itemWriter = new ItemWriter<>() {
+            @Override
+            public void write(List<? extends Account> items) throws Exception {
+                log.info("Complete One Chunk");
+            }
+        };
+        return itemWriter;
+    }
 }
--- src/main/java/com/yapp/project/batch/scheduler/ReportScheduler.java
+++ src/main/java/com/yapp/project/batch/scheduler/ReportScheduler.java
@@ -1,2 +1,36 @@
-package com.yapp.project.batch.scheduler;public class ReportScheduler {
+package com.yapp.project.batch.scheduler;
+
+import com.yapp.project.aux.alert.AlertService;
+import com.yapp.project.aux.alert.SlackChannel;
+import com.yapp.project.aux.common.DateUtil;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionException;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReportScheduler {
+    private final Job missionJob;
+    private final JobLauncher jobLauncher;
+    private final AlertService alertService;
+
+    public ReportScheduler(JobLauncher jobLauncher, @Qualifier("makeWeekReportJob") Job weekReportJob, AlertService alertService){
+        this.missionJob = weekReportJob;
+        this.jobLauncher = jobLauncher;
+        this.alertService = alertService;
+    }
+
+    @Scheduled(cron = "0 0 17 * * 3")
+    public void missionBatchExecuteJob() throws JobExecutionException{
+        alertService.slackSendMessage(SlackChannel.BATCH,
+                ":arrow_forward:주 리포트 발급 배치작업 시작합니다.");
+        jobLauncher.run(missionJob, new JobParametersBuilder()
+                .addString("makeWeekReportDate", DateUtil.KST_LOCAL_DATETIME_NOW().toString())
+                .toJobParameters());
+        alertService.slackSendMessage(SlackChannel.BATCH,
+                ":ballot_box_with_check:주 리포트 발급 배치작업 마쳤습니다.");
+    }
 }
