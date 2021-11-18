6826a82660f80a92a89dcecefa9fcfd6457bec81
solchan98 <dev.sol0427@gmail.com>
2021-11-18 17:59:06 +0900

    [YAPPIOS-151] 리포트 스케줄링 관련 메시지 상수 처리

--- src/main/java/com/yapp/project/aux/content/ReportContent.java
+++ src/main/java/com/yapp/project/aux/content/ReportContent.java
@@ -10,4 +10,12 @@ public class ReportContent {
     public static final String DAY_ROUTINE_RATE_OK = "요일별 루틴 수행률 조회 성공.";
     public static final String WEEK_REPORT_OK = "주 리포트를 성공적으로 조회하였습니다";
     public static final String WEEK_REPORT_NOT_FOUND = "요청하신 요일이 일요일이 맞는지 확인하세요.";
+    public static final String WEEK_REPORT_PUSH_START = ":arrow_forward:주 리포트 알림 푸시 시작합니다.";
+    public static final String WEEK_REPORT_PUSH_END = ":ballot_box_with_check:주 리포트 알림 푸시 마쳤습니다.";
+    public static final String MONTH_REPORT_PUSH_START = ":arrow_forward:월 리포트 알림 푸시 시작합니다.";
+    public static final String MONTH_REPORT_PUSH_END = ":ballot_box_with_check:월 리포트 알림 푸시 마쳤습니다.";
+    public static final String WEEK_REPORT_CREATE_START = ":arrow_forward:주 리포트 발급 배치작업 시작합니다.";
+    public static final String WEEK_REPORT_CREATE_END = ":ballot_box_with_check:주 리포트 발급 배치작업 마쳤습니다.";
+    public static final String MONTH_REPORT_CREATE_START = ":arrow_forward:월 리포트 발급 배치작업 시작합니다.";
+    public static final String MONTH_REPORT_CREATE_END = ":ballot_box_with_check:월 리포트 발급 배치작업 마쳤습니다.";
 }
--- src/main/java/com/yapp/project/batch/scheduler/ReportScheduler.java
+++ src/main/java/com/yapp/project/batch/scheduler/ReportScheduler.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import static com.yapp.project.aux.content.ReportContent.*;
+
 @Component
 public class ReportScheduler {
     private final Job weekReportJob;
@@ -28,23 +30,19 @@ public class ReportScheduler {
 
     @Scheduled(cron = "0 0 17 * * 3")
     public void makeWeekReportJob() throws JobExecutionException{
-        alertService.slackSendMessage(SlackChannel.BATCH,
-                ":arrow_forward:주 리포트 발급 배치작업 시작합니다.");
+        alertService.slackSendMessage(SlackChannel.BATCH, WEEK_REPORT_CREATE_START);
         jobLauncher.run(weekReportJob, new JobParametersBuilder()
                 .addString("makeWeekReportDate", DateUtil.KST_LOCAL_DATETIME_NOW().toString())
                 .toJobParameters());
-        alertService.slackSendMessage(SlackChannel.BATCH,
-                ":ballot_box_with_check:주 리포트 발급 배치작업 마쳤습니다.");
+        alertService.slackSendMessage(SlackChannel.BATCH, WEEK_REPORT_CREATE_END);
     }
 
     @Scheduled(cron = "0 0 18 1 * ?")
     public void makeMonthReportJob() throws JobExecutionException{
-        alertService.slackSendMessage(SlackChannel.BATCH,
-                ":arrow_forward:월 리포트 발급 배치작업 시작합니다.");
+        alertService.slackSendMessage(SlackChannel.BATCH, MONTH_REPORT_CREATE_START);
         jobLauncher.run(monthReportJob, new JobParametersBuilder()
                 .addString("makeMonthReportDate", DateUtil.KST_LOCAL_DATETIME_NOW().toString())
                 .toJobParameters());
-        alertService.slackSendMessage(SlackChannel.BATCH,
-                ":ballot_box_with_check:월 리포트 발급 배치작업 마쳤습니다.");
+        alertService.slackSendMessage(SlackChannel.BATCH, MONTH_REPORT_CREATE_END);
     }
 }
