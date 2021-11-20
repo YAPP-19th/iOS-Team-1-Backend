09c5493ae51a8172352ebda5761c61001c2e7690
VIXXPARK <vixx170627@gmail.com>
2021-11-20 15:18:20 +0900

    [YAPPIOS-145] 비밀번호 정규식 - https://vixxcode.tistory.com/192 내용에 따라 적용

--- src/main/java/com/yapp/project/account/util/PasswordUtil.java
+++ src/main/java/com/yapp/project/account/util/PasswordUtil.java
@@ -1,10 +1,12 @@
 package com.yapp.project.account.util;
 
+import java.util.regex.Pattern;
+
 public class PasswordUtil {
     private PasswordUtil(){
     }
+    private static final Pattern PATTERN = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[!@#$%^&+=])(?=\\S+$).{8,}$");
     public static boolean validPassword(String password){
-        String pattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[!@#$%^&+=])(?=\\S+$).{8,}$";
-        return password.matches(pattern);
+        return PATTERN.matcher(password).matches();
     }
 }
