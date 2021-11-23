23f1163c7f0df175fd7e70d78426c30d5a6c037e
VIXXPARK <vixx170627@gmail.com>
2021-11-23 16:59:50 +0900

    [YAPPIOS-178] 이메일 검증 정규식 추가

--- src/test/java/com/yapp/project/account/util/RegexUtilTest.java
+++ src/test/java/com/yapp/project/account/util/RegexUtilTest.java
@@ -2,36 +2,36 @@ package com.yapp.project.account.util;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
-class PasswordUtilTest {
+class RegexUtilTest {
 
     @Test
     void 정규식에_맞게_비밀번호를_작성했을_때(){
         String password = "Test12#$%";
-        assertThat(PasswordUtil.validPassword(password)).isTrue();
+        assertThat(RegexUtil.validPassword(password)).isTrue();
     }
 
     @Test
     void 글자수가_8글자_미만일_때(){
         String password = "Test12";
-        assertThat(PasswordUtil.validPassword(password)).isFalse();
+        assertThat(RegexUtil.validPassword(password)).isFalse();
     }
 
     @Test
     void 숫자와_특수문자가_없을때(){
         String password = "testtest";
-        assertThat(PasswordUtil.validPassword(password)).isFalse();
+        assertThat(RegexUtil.validPassword(password)).isFalse();
     }
 
     @Test
     void 숫자가_없을때(){
         String password = "test!!@@";
-        assertThat(PasswordUtil.validPassword(password)).isFalse();
+        assertThat(RegexUtil.validPassword(password)).isFalse();
     }
 
     @Test
     void 특수문자가_없을때(){
         String password = "test1234";
-        assertThat(PasswordUtil.validPassword(password)).isFalse();
+        assertThat(RegexUtil.validPassword(password)).isFalse();
     }
 
 }
