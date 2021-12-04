801e26aebea8a43576a7b9c15ca7044424f91bd1
solchan98 <dev.sol0427@gmail.com>
2021-12-04 16:39:13 +0900

    [YAPPIOS-190] 루틴 및 회고 N+1 해결

--- src/main/java/com/yapp/project/retrospect/domain/RetrospectRepository.java
+++ src/main/java/com/yapp/project/retrospect/domain/RetrospectRepository.java
@@ -2,7 +2,9 @@ package com.yapp.project.retrospect.domain;
 
 import com.yapp.project.account.domain.Account;
 import com.yapp.project.routine.domain.Routine;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -13,6 +15,8 @@ import java.util.Optional;
 public interface RetrospectRepository extends JpaRepository<Retrospect, Long> {
     Optional<Retrospect> findByRoutineAndDate(Routine routine, LocalDate date);
 
+    @EntityGraph(attributePaths = {"routine", "routine.days"})
+    @Query("select m from Retrospect m where m.date= :date and m.routine.account= :account")
     List<Retrospect> findAllByDateAndRoutineAccount(LocalDate date, Account account);
 
     List<Retrospect> findAllByIsReportIsFalseAndRoutineAccount(Account account);
