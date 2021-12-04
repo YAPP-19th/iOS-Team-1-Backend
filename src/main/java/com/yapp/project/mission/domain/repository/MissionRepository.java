2a1b70e7e3eff8c0e5180bce7d97e1ab5e1c6bdb
VIXXPARK <vixx170627@gmail.com>
2021-12-04 14:10:08 +0900

    [YAPPIOS-188] JPA N+1 문제 해결 - https://vixxcode.tistory.com/205 - 연관 관계가 있는 경우 JPA N+1 문제가 있는데 이를 해결하기 위해 EntityGraph를 사용함

--- src/main/java/com/yapp/project/mission/domain/repository/MissionRepository.java
+++ src/main/java/com/yapp/project/mission/domain/repository/MissionRepository.java
@@ -3,7 +3,9 @@ package com.yapp.project.mission.domain.repository;
 import com.yapp.project.account.domain.Account;
 import com.yapp.project.mission.domain.Mission;
 import com.yapp.project.mission.domain.dao.MissionOrganization;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -11,13 +13,34 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MissionRepository extends JpaRepository<Mission,Long> {
+    @EntityGraph(attributePaths = {"organization"})
+    @Query("SELECT DISTINCT m FROM Mission m WHERE m.account = ?1")
     ArrayList<MissionOrganization> findMissionByAccountAndIsFinishIsFalseAndIsDeleteIsFalse(Account account);
+
+    @EntityGraph(attributePaths = {"organization"})
+    @Query("SELECT DISTINCT m FROM Mission m ")
     List<Mission> findAllByAccountAndIsFinishIsFalseAndIsDeleteIsFalse(Account account);
+
+    @EntityGraph(attributePaths = {"organization"})
+    @Query("SELECT DISTINCT m FROM Mission m ")
     List<Mission> findAllByAccountAndIsDeleteIsFalseAndIsFinishIsTrue(Account account);
+
+    @EntityGraph(attributePaths = {"organization"})
+    @Query("SELECT DISTINCT m FROM Mission m ")
     List<Mission> findAllByIsDeleteIsFalseAndIsAlarmIsTrueAndStartTimeEquals(LocalTime startTime);
+
+    @EntityGraph(attributePaths = {"organization"})
+    @Query("SELECT DISTINCT m FROM Mission m ")
     List<Mission> findAllByIsDeleteIsFalse();
+
+    @EntityGraph(attributePaths = {"organization"})
+    @Query("SELECT DISTINCT m FROM Mission m ")
     List<Mission> findAllByIsDeleteIsFalseAndIsFinishIsFalse();
+
+    @EntityGraph(attributePaths = {"organization"})
+    @Query("SELECT DISTINCT m FROM Mission m WHERE m.account = ?1")
     List<Mission> findAllByAccount(Account account);
+
     Optional<Mission> findMissionByAccountAndId(Account account, Long missionId);
     Optional<Mission> findMissionByAccountAndOrganization_IdAndIsFinishIsFalseAndIsDeleteIsFalse(Account account, Long organizationId);
 }
--- src/test/java/com/yapp/project/organization/domain/repository/OrganizationRepositoryTest.java
+++ src/test/java/com/yapp/project/organization/domain/repository/OrganizationRepositoryTest.java
@@ -1,6 +1,17 @@
 package com.yapp.project.organization.domain.repository;
 
 import static com.yapp.project.aux.test.organization.OrganizationTemplate.*;
+
+import com.yapp.project.account.domain.Account;
+import com.yapp.project.account.domain.repository.AccountRepository;
+import com.yapp.project.aux.test.account.AccountTemplate;
+import com.yapp.project.aux.test.capture.CaptureTemplate;
+import com.yapp.project.aux.test.mission.MissionTemplate;
+import com.yapp.project.aux.test.organization.OrganizationTemplate;
+import com.yapp.project.capture.domain.Capture;
+import com.yapp.project.capture.domain.repository.CaptureRepository;
+import com.yapp.project.mission.domain.Mission;
+import com.yapp.project.mission.domain.repository.MissionRepository;
 import com.yapp.project.organization.domain.Organization;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +19,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -19,11 +32,72 @@ class OrganizationRepositoryTest {
     @Autowired
     private OrganizationRepository organizationRepository;
 
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private MissionRepository missionRepository;
+
+    @Autowired
+    private CaptureRepository captureRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
     @Test
     void test_카테고리로_그룹_조회(){
         Organization organization = makeTestOrganization();
         organizationRepository.save(organization);
         List<Organization> organizations = organizationRepository.findByCategoryAndMore(CATEGORY);
         assertThat(organizations.get(0).getCategory()).isEqualTo(CATEGORY);
+    }
+
+    @Test
+    void test_NOT_IN_쿼리확인() {
+        Account account = AccountTemplate.makeTestAccountForIntegration();
+        List<Organization> organizations = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            organizations.add(OrganizationTemplate.makeTestOrganizationForIntegration("테스트"+i));
+        }
+        Account newAccount = accountRepository.save(account);
+        List<Organization> organizationList = organizationRepository.saveAll(organizations);
+        List<Mission> missions = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            missions.add(MissionTemplate.makeMissionForIntegration(newAccount,organizationList.get(i)));
+        }
+        List<Mission> missionList = missionRepository.saveAll(missions);
+        List<Capture> captures = new ArrayList<>();
+        for (Mission mission: missionList) {
+            captures.add(CaptureTemplate.makeCapture(mission));
+        }
+        captureRepository.saveAll(captures);
+        List<Long> excludeOrganizationIds = List.of(organizationList.get(0).getId());
+        entityManager.clear();
+        List<Organization> totalOrganizations = organizationRepository.findAll();
+        List<Organization> orgs = organizationRepository.findOrganizationsNotIn(excludeOrganizationIds);
+        assertThat(totalOrganizations.size() - orgs.size()).isEqualTo(1);
+        Organization organization = orgs.get(orgs.size()-1);
+        assertThat(organization.getMissions().size()).isOne();
+
+    }
+
+    @Test
+    void test_JPA_N_PLUS_1_문제를_해결하기_위한_테스트() {
+        Account account = AccountTemplate.makeTestAccountForIntegration();
+        List<Organization> organizations = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            organizations.add(OrganizationTemplate.makeTestOrganizationForIntegration("테스트"+i));
+        }
+        Account newAccount = accountRepository.save(account);
+        List<Organization> organizationList = organizationRepository.saveAll(organizations);
+        List<Mission> missions = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            missions.add(MissionTemplate.makeMissionForIntegration(newAccount,organizationList.get(i)));
+        }
+        List<Mission> missionList = missionRepository.saveAll(missions);
+        entityManager.clear();
+
+        List<Mission> myMissions = missionRepository.findAllByAccount(newAccount);
+        assertThat(myMissions.size()).isEqualTo(10);
     }
 }
