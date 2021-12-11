b84b747f78b6209f6f9d8134a325238310bb9717
solchan98 <dev.sol0427@gmail.com>
2021-12-12 05:15:00 +0900

    [YAPPIOS-210] 월 리포트 카테고리별 비율 계산 로직 추가 및 응답 형식 수정

--- src/main/java/com/yapp/project/report/controller/ReportController.java
+++ src/main/java/com/yapp/project/report/controller/ReportController.java
@@ -3,7 +3,6 @@ package com.yapp.project.report.controller;
 import com.yapp.project.aux.common.AccountUtil;
 import com.yapp.project.report.domain.dto.ReportDTO;
 import com.yapp.project.report.service.ReportService;
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,13 +14,14 @@ import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/report")
-@Api(tags = "리포트")
+@RequestMapping("/api/v1/report/")
 public class ReportController {
 
     private final ReportService reportService;
 
-    @ApiOperation(value = "월 리포트 조회", notes = "월 리포트 조회하기. \n 월 리포트의 년도와 월을 넘겨주세요. \n ex) 2021/10")
+    @ApiOperation(value = "월 리포트 조회", notes = "월 리포트 조회하기. \n 월 리포트의 년도와 월을 넘겨주세요. \n ex) 2021/10" +
+            "\n 응답 예시에 additionalProp1, ..2는 카테고리 명입니다. additionalProp1대신에 아래 키로 받으시면 됩니다." +
+            "\n [miracle, self, health, daily, etc]")
     @GetMapping("/month/{year}/{month}")
     public ReportDTO.ResponseMonthReportMessage getMonthReportByYearAndMonth(
             @PathVariable Integer year, @PathVariable Integer month) {
@@ -33,6 +33,4 @@ public class ReportController {
     public ReportDTO.ResponseWeekReportMessage getWeekReport(@PathVariable String lastDate) {
         return reportService.getWeekReportLastDate(AccountUtil.getAccount(), LocalDate.parse(lastDate));
     }
-
-
 }
