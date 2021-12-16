cf2eb13649b9880a5076c7f362d7de6c4597029e
solchan98 <dev.sol0427@gmail.com>
2021-12-16 20:32:31 +0900

    [YAPPIOS-221] JenkinsFile 수정

--- run.zsh
+++ run.zsh
@@ -1,4 +1,4 @@
 sudo docker rm -f app
 sudo docker pull yapp19th/develop
-sudo docker run --name app -d -v /home/image:/home/image -p 8000:8000 yapp19th/develop
+sudo docker run --name app -d -v /home/image:/home/image -p 8000:8000 --net devsol0427_minning_network yapp19th/develop
 sudo docker rmi -f $(sudo docker images -f "dangling=true" -q)
