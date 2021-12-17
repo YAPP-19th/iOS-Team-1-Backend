05be280855dac541a9746d37ae3d0ebeb970ffae
solchan <64524916+solchan98@users.noreply.github.com>
2021-12-18 00:25:48 +0900

    [YAPPIOS-231] Swagger 설명 수정

--- src/main/java/com/yapp/project/report/controller/ReportController.java
+++ src/main/java/com/yapp/project/report/controller/ReportController.java
@@ -23,7 +23,7 @@ public class ReportController {
 
     @ApiOperation(value = "월 리포트 조회", notes = "월 리포트 조회하기. \n 월 리포트의 년도와 월을 넘겨주세요. \n ex) 2021/10" +
             "\n 응답 예시에 resultByCategory에는 카테고리별 순서대로 들어가 있습니다. 순서는 다음과 같습니다." +
-            "\n[miracle, self, health, daily, morning, etc]")
+            "\n[miracle, self, health, daily, etc]")
     @GetMapping("/month/{year}/{month}")
     public ReportDTO.ResponseMonthReportMessage getMonthReportByYearAndMonth(
             @PathVariable Integer year, @PathVariable Integer month) {
